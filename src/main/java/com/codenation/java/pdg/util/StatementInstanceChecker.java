c785de572c4b768200eafe5eb50a0994bd9adf9f
Ferooz Khan <ferooz@codenation.co.in>
2017-02-15 17:47:39 +0530

    deleted instanceOf classes and added enum

--- src/main/java/com/codenation/java/pdg/util/StatementInstanceChecker.java
+++
@@ -1,7 +0,0 @@
-package com.codenation.java.pdg.util;
-
-import org.eclipse.jdt.core.dom.Statement;
-
-public interface StatementInstanceChecker {
-	public boolean instanceOf(Statement statement);
-}
